403b69ef68e8c2e2f85057685d7740c5bbaee78d
fadrian06 <franyeradriansanchez@gmail.com>
2025-01-13 23:13:20 -0400

    Fix `recuperarcon.php`

--- recuperarcon.php
+++ recuperarcon.php
@@ -2,72 +2,65 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=, initial-scale=1.0">
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <link rel="stylesheet" href="">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=, initial-scale=1.0">
+  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+  <title>Document</title>
 </head>
 
 <body>
-    <header>
-        <p>en entos momentos no es posible</p>
-        <h1>Recuperarcion de la Contraseña</h1>
-    </header>
+  <header>
+    <p>en entos momentos no es posible</p>
+    <h1>Recuperarcion de la Contraseña</h1>
+  </header>
 
+  <main>
+    <section>
+      <h2>datos a Completar</h2>
+      <form action="a" method="post">
+        <p>
+          <label>
+            <i class="bx bx-user-circle"></i>
+            Nombre: <input type="text" name="Nombre" placeholder="Nombre Completo" required>
+          </label>
+        </p>
+        <p>
+          <label>
+            <i class="bx bx-user-circle"></i>
+            Apellidos: <input type="text" name="Apellidos" placeholder="Apellidos">
+          </label>
+        </p>
+        <p>
+          <label>
+            <i class="bx bx-user-circle"></i>
+            Email: <input type="text" name="Email" placeholder="Email">
+          </label>
+        </p>
+        <p>
+          <label>
+            <i class="bx bx-user-circle"></i>
+            Teléfono: <input type="text" name="Teléfono" placeholder="Telefono">
+          </label>
+        </p>
 
-    <main>
-        <section>
-            <h2>datos a Completar</h2>
-            <form action="a" method="post">
-                <p>
-                    <label>
-                        <i class="bx bx-user-circle"></i>
-                        Nombre: <input type="text" name="Nombre" placeholder="Nombre Completo" required>
-                    </label>
-                </p>
-                <p>
-                    <label>
-                        <i class="bx bx-user-circle"></i>
-                        Apellidos: <input type="text" name="Apellidos" placeholder="Apellidos">
-                    </label>
-                </p>
-                <p>
-                    <label>
-                        <i class="bx bx-user-circle"></i>
-                        Email: <input type="text" name="Email" placeholder="Email">
-                    </label>
-                </p>
-                <p>
-                    <label>
-                        <i class="bx bx-user-circle"></i>
-                        Teléfono: <input type="text" name="Teléfono" placeholder="Telefono">
-                    </label>
-                </p>
+        <h3>Selecciona una occión</h3>
+        <p>
+          <label>
+            Opcion 1 <input id="radio1" type="radio" name="radioBoton">
+          </label>
+        </p>
 
-                <h3>Selecciona una occión</h3>
-                <p>
-                    <label>
-                        Opcion 1 <input id="radio1" type="radio" name="radioBoton">
-                    </label>
-                </p>
-
-                <p>
-                    <label>
-                        Opcion 2<input id="radio2" type="radio" name="radioBoton">
-                    </label>
-                </p>
-                <input type="submit" value="Enviar datos" formtarget="_blank  " >
-                <input type="reset" value="Restablecer">
-            </form>
-
-
-        </section>
-
-    </main>
-    <a href="login.php">Volver a pagina principal </a>
-
-
+        <p>
+          <label>
+            Opcion 2<input id="radio2" type="radio" name="radioBoton">
+          </label>
+        </p>
+        <input type="submit" value="Enviar datos" formtarget="_blank  ">
+        <input type="reset" value="Restablecer">
+      </form>
+    </section>
+  </main>
+  <a href="login.php">Volver a pagina principal </a>
 </body>
 
 </html>
